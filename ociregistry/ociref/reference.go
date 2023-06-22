ea6c178cf6d94be48e47a8b31d3cc6481195d8ba
Roger Peppe <rogpeppe@gmail.com>
2023-06-22 13:33:10 +0100

    ociref: new package
    This is the start of implementing the package described in issue #7.
    
    I started with the regular expressions defined in the [distribution reference package](https://pkg.go.dev/github.com/distribution/distribution/v3/reference) at commit 97b1d649c4938d0f608d96454d6a8326b1f96acd and worked from there.
    
    I made a few changes, some or all of which may be misguided :)
    
    The most significant of the changes is that a reference of the
    form `a/b` is parsed as the (relative) repository name `a/b` rather
    than host `a` followed by repository `b`. This means that it's always
    unambiguous whether a reference is relative or absolute.
    
    The distinction is made based on whether the first component is
    unambiguously a host name; specifically one of:
    - it contains a dot
    - it looks like an IPV6 address
    - it has an explicit port

---
+++ ociregistry/ociref/reference.go
@@ -0,0 +1,192 @@
+// Package ociref implements cross-registry OCI operations.
+package ociref
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"cuelabs.dev/go/oci/ociregistry"
+)
+
+// The following regular expressions derived from code in the
+// [github.com/distribution/distribution/v3/reference] package.
+const (
+	// alphanumeric defines the alphanumeric atom, typically a
+	// component of names. This only allows lower case characters and digits.
+	alphanumeric = `[a-z0-9]+`
+
+	// separator defines the separators allowed to be embedded in name
+	// components. This allows one period, one or two underscore and multiple
+	// dashes. Repeated dashes and underscores are intentionally treated
+	// differently. In order to support valid hostnames as name components,
+	// supporting repeated dash was added. Additionally double underscore is
+	// now allowed as a separator to loosen the restriction for previously
+	// supported names.
+	// TODO the distribution spec doesn't allow these variations.
+	separator = `(?:[._]|__|[-]+)`
+
+	// domainNameComponent restricts the registry domain component of a
+	// repository name to start with a component as defined by DomainRegexp.
+	domainNameComponent = `(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)`
+
+	// ipv6address are enclosed between square brackets and may be represented
+	// in many ways, see rfc5952. Only IPv6 in compressed or uncompressed format
+	// are allowed, IPv6 zone identifiers (rfc6874) or Special addresses such as
+	// IPv4-Mapped are deliberately excluded.
+	ipv6address = `(?:\[[a-fA-F0-9:]+\])`
+
+	// optionalPort matches an optional port-number including the port separator
+	// (e.g. ":80").
+	port = `[0-9]+`
+
+	// domainName defines the structure of potential domain components
+	// that may be part of image names. This is purposely a subset of what is
+	// allowed by DNS to ensure backwards compatibility with Docker image
+	// names. This includes IPv4 addresses on decimal format.
+	//
+	// Note: we purposely exclude domain names without dots here,
+	// because otherwise we can't tell if the first component is
+	// a host name or not when it doesn't have a port.
+	// When it does have a port, the distinction is clear.
+	//
+	domainName = `(?:` + domainNameComponent + `(?:\.` + domainNameComponent + `)+` + `)`
+
+	// host defines the structure of potential domains based on the URI
+	// Host subcomponent on rfc3986. It may be a subset of DNS domain name,
+	// or an IPv4 address in decimal format, or an IPv6 address between square
+	// brackets (excluding zone identifiers as defined by rfc6874 or special
+	// addresses such as IPv4-Mapped).
+	host = `(?:` + domainName + `|` + ipv6address + `)`
+
+	// allowed by the URI Host subcomponent on rfc3986 to ensure backwards
+	// compatibility with Docker image names.
+	// Note: that we require the port when the host name looks like a regular
+	// name component.
+	domainAndPort = `(?:` + host + `(?:` + `:` + port + `)?` + `|` + domainNameComponent + `:` + port + `)`
+
+	// pathComponent restricts path-components to start with an alphanumeric
+	// character, with following parts able to be separated by a separator
+	// (one period, one or two underscore and multiple dashes).
+	pathComponent = `(?:` + alphanumeric + `(?:` + separator + alphanumeric + `)*` + `)`
+
+	// repoName matches the name of a repository. It consists of one
+	// or more forward slash (/) delimited path-components:
+	//
+	//	pathComponent[[/pathComponent] ...] // e.g., "library/ubuntu"
+	repoName = pathComponent + `(?:` + `/` + pathComponent + `)*`
+)
+
+var referencePat = regexp.MustCompile(
+	`^(?:` +
+		`(?:` + `(` + domainAndPort + `)` + `/` + `)?` + // capture 1: host
+		`(` + repoName + `)` + // capture 2: repository name
+		`(?:` + `:([^@]+))?` + // capture 3: tag; rely on Go logic to test validity.
+		`(?:` + `@(.+))?` + // capture 4: digest; rely on go-digest to find issues
+		`)$`,
+)
+
+// Reference represents an entry in an OCI repository.
+type Reference struct {
+	// Host holds the host name of the registry
+	// within which the repository is stored. This might
+	// be empty.
+	Host string
+
+	// Repository holds the repository name.
+	Repository string
+
+	// Tag holds the TAG part of a :TAG or :TAG@DIGEST reference.
+	// When Digest is set as well as Tag, the tag will be verified
+	// to exist and have the expected digest.
+	Tag string
+
+	// Digest holds the DIGEST part of an @DIGEST reference
+	// or of a :TAG@DIGEST reference.
+	Digest ociregistry.Digest
+}
+
+// Parse parses a reference string that must include
+// a host name component.
+//
+// It is represented in string form as HOST/NAME[:TAG|@DIGEST]
+// form: the same syntax accepted by "docker pull".
+// Unlike "docker pull" however, there is no default registry: when
+// presented with a bare repository name, Parse will return an error.
+func Parse(refStr string) (Reference, error) {
+	ref, err := ParseRelative(refStr)
+	if err != nil {
+		return Reference{}, err
+	}
+	if ref.Host == "" {
+		return Reference{}, fmt.Errorf("reference does not contain host name")
+	}
+	return ref, nil
+}
+
+// ParseRelative parses a reference string that may
+// or may not include a host name component.
+//
+// It is represented in string form as [HOST/]NAME[:TAG|@DIGEST]
+// form: the same syntax accepted by "docker pull".
+// Unlike "docker pull" however, there is no default registry: when
+// presented with a bare repository name, the Host field will be empty.
+func ParseRelative(refStr string) (Reference, error) {
+	m := referencePat.FindStringSubmatch(refStr)
+	if m == nil {
+		return Reference{}, fmt.Errorf("invalid reference syntax (%q)", refStr)
+	}
+	var ref Reference
+	ref.Host, ref.Repository, ref.Tag, ref.Digest = m[1], m[2], m[3], ociregistry.Digest(m[4])
+	// Check lengths and digest: we don't check these as part of the regexp
+	// because it's more efficient to do it in Go and we get
+	// nicer error messages as a result.
+	if len(ref.Digest) > 0 {
+		if err := ref.Digest.Validate(); err != nil {
+			return Reference{}, fmt.Errorf("invalid digest %q: %v", ref.Digest, err)
+		}
+	}
+	if len(ref.Tag) > 0 {
+		if len(ref.Tag) > 127 {
+			return Reference{}, fmt.Errorf("tag too long")
+		}
+		if !isWord(ref.Tag[0]) {
+			return Reference{}, fmt.Errorf("tag %q does not start with word character", ref.Tag)
+		}
+		for i := 1; i < len(ref.Tag); i++ {
+			c := ref.Tag[i]
+			if !isWord(c) && c != '.' && c != '-' {
+				return Reference{}, fmt.Errorf("tag %q contains invalid invalid character %q", ref.Tag, c)
+			}
+		}
+	}
+	if len(ref.Repository) > 255 {
+		return Reference{}, fmt.Errorf("repository name too long")
+	}
+	return ref, nil
+}
+
+func isWord(c byte) bool {
+	return c == '_' || ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9')
+}
+
+// String returns the string form of a reference in the form
+//	[HOST/]NAME[:TAG|@DIGEST]
+func (ref Reference) String() string {
+	var buf strings.Builder
+	buf.Grow(len(ref.Host) + 1 + len(ref.Repository) + 1 + len(ref.Tag) + 1 + len(ref.Digest))
+	if ref.Host != "" {
+		buf.WriteString(ref.Host)
+		buf.WriteByte('/')
+	}
+	buf.WriteString(ref.Repository)
+	if len(ref.Tag) > 0 {
+		buf.WriteByte(':')
+		buf.WriteString(ref.Tag)
+	}
+	if len(ref.Digest) > 0 {
+		buf.WriteByte('@')
+		buf.WriteString(string(ref.Digest))
+	}
+	return buf.String()
+}
---
+++ ociregistry/ociref/reference_test.go
@@ -0,0 +1,364 @@
+package ociref
+
+import (
+	_ "crypto/sha256"
+	_ "crypto/sha512"
+	"fmt"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/go-quicktest/qt"
+)
+
+var parseReferenceTests = []struct {
+	testName string
+	// input is the repository name or name component testcase
+	input string
+	// err is the error expected from Parse, or nil
+	wantErr string
+	wantRef Reference
+}{
+	{
+		input: "test_com",
+		wantRef: Reference{
+			Repository: "test_com",
+		},
+	},
+	{
+		input: "test.com:tag",
+		wantRef: Reference{
+			Repository: "test.com",
+			Tag:        "tag",
+		},
+	},
+	{
+		input: "test.com:5000",
+		wantRef: Reference{
+			Repository: "test.com",
+			Tag:        "5000",
+		},
+	},
+	{
+		input: "test.com/repo:tag",
+		wantRef: Reference{
+			Host:       "test.com",
+			Repository: "repo",
+			Tag:        "tag",
+		},
+	},
+	{
+		input: "test:5000/repo",
+		wantRef: Reference{
+			Host:       "test:5000",
+			Repository: "repo",
+		},
+	},
+	{
+		input: "test:5000/repo:tag",
+		wantRef: Reference{
+			Host:       "test:5000",
+			Repository: "repo",
+			Tag:        "tag",
+		},
+	},
+	{
+		input: "test:5000/repo@sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		wantRef: Reference{
+			Host:       "test:5000",
+			Repository: "repo",
+			Digest:     "sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		},
+	},
+	{
+		input: "test:5000/repo:tag@sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		wantRef: Reference{
+			Host:       "test:5000",
+			Repository: "repo",
+			Tag:        "tag",
+			Digest:     "sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		},
+	},
+	{
+		input: "test:5000/repo",
+		wantRef: Reference{
+			Host:       "test:5000",
+			Repository: "repo",
+		},
+	},
+	{
+		testName: "EmptyString",
+		input:    "",
+		wantErr:  `invalid reference syntax`,
+	},
+	{
+		input:   ":justtag",
+		wantErr: `invalid reference syntax`,
+	},
+	{
+		input:   "@sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		wantErr: `invalid reference syntax`,
+	},
+	{
+		input:   "repo@sha256:ffffffffffffffffffffffffffffffffff",
+		wantErr: `invalid digest "sha256:ffffffffffffffffffffffffffffffffff": invalid checksum digest length`,
+	},
+	{
+		input:   "validname@invalidDigest:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		wantErr: `invalid digest "invalidDigest:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff": invalid checksum digest format`,
+	},
+	{
+		input:   "Uppercase:tag",
+		wantErr: `invalid reference syntax`,
+	},
+	// FIXME "Uppercase" is incorrectly handled as a domain-name here, therefore passes.
+	// See https://github.com/distribution/distribution/pull/1778, and https://github.com/docker/docker/pull/20175
+	// {
+	//	input: "Uppercase/lowercase:tag",
+	//	err:   ErrNameContainsUppercase,
+	// },
+	{
+		input:   "test:5000/Uppercase/lowercase:tag",
+		wantErr: `tag "5000/Uppercase/lowercase:tag" contains invalid invalid character '/'`,
+	},
+	{
+		input: "lowercase:Uppercase",
+		wantRef: Reference{
+			Repository: "lowercase",
+			Tag:        "Uppercase",
+		},
+	},
+	{
+		testName: "RepoTooLong",
+		input:    strings.Repeat("a/", 128) + "a:tag",
+		wantErr:  `repository name too long`,
+	},
+	{
+		testName: "RepoAlmostTooLong",
+		input:    strings.Repeat("a/", 127) + "a:tag-puts-this-over-max",
+		wantRef: Reference{
+			// Note: docker/reference parses Host as "a".
+			Repository: strings.Repeat("a/", 127) + "a",
+			Tag:        "tag-puts-this-over-max",
+		},
+	},
+	{
+		input:   "aa/asdf$$^/aa",
+		wantErr: `invalid reference syntax`,
+	},
+	{
+		input: "sub-dom1.foo.com/bar/baz/quux",
+		wantRef: Reference{
+			Host:       "sub-dom1.foo.com",
+			Repository: "bar/baz/quux",
+		},
+	},
+	{
+		input: "sub-dom1.foo.com/bar/baz/quux:some-long-tag",
+		wantRef: Reference{
+			Host:       "sub-dom1.foo.com",
+			Repository: "bar/baz/quux",
+			Tag:        "some-long-tag",
+		},
+	},
+	{
+		input: "b.gcr.io/test.example.com/my-app:test.example.com",
+		wantRef: Reference{
+			Host:       "b.gcr.io",
+			Repository: "test.example.com/my-app",
+			Tag:        "test.example.com",
+		},
+	},
+	{
+		input: "xn--n3h.com/myimage:xn--n3h.com", // ☃.com in punycode
+		wantRef: Reference{
+			Host:       "xn--n3h.com",
+			Repository: "myimage",
+			Tag:        "xn--n3h.com",
+		},
+	},
+	{
+		input: "xn--7o8h.com/myimage:xn--7o8h.com@sha512:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", // 🐳.com in punycode
+		wantRef: Reference{
+			Host:       "xn--7o8h.com",
+			Repository: "myimage",
+			Tag:        "xn--7o8h.com",
+			Digest:     "sha512:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		},
+	},
+	{
+		input: "foo_bar.com:8080",
+		wantRef: Reference{
+			Repository: "foo_bar.com",
+			Tag:        "8080",
+		},
+	},
+	{
+		input: "foo.com/bar:8080",
+		wantRef: Reference{
+			Host:       "foo.com",
+			Repository: "bar",
+			Tag:        "8080",
+		},
+	},
+	{
+		input: "foo/foo_bar.com:8080",
+		wantRef: Reference{
+			Repository: "foo/foo_bar.com",
+			Tag:        "8080",
+		},
+	},
+	{
+		input: "192.168.1.1",
+		wantRef: Reference{
+			Repository: "192.168.1.1",
+		},
+	},
+	{
+		input: "192.168.1.1:tag",
+		wantRef: Reference{
+			Repository: "192.168.1.1",
+			Tag:        "tag",
+		},
+	},
+	{
+		input: "192.168.1.1:5000",
+		wantRef: Reference{
+			Repository: "192.168.1.1",
+			Tag:        "5000",
+		},
+	},
+	{
+		input: "192.168.1.1/repo",
+		wantRef: Reference{
+			Host:       "192.168.1.1",
+			Repository: "repo",
+		},
+	},
+	{
+		input: "192.168.1.1:5000/repo",
+		wantRef: Reference{
+			Host:       "192.168.1.1:5000",
+			Repository: "repo",
+		},
+	},
+	{
+		input: "192.168.1.1:5000/repo:5050",
+		wantRef: Reference{
+			Host:       "192.168.1.1:5000",
+			Repository: "repo",
+			Tag:        "5050",
+		},
+	},
+	{
+		input:   "[2001:db8::1]",
+		wantErr: `invalid reference syntax`,
+	},
+	{
+		input:   "[2001:db8::1]:5000",
+		wantErr: `invalid reference syntax`,
+	},
+	{
+		input:   "[2001:db8::1]:tag",
+		wantErr: `invalid reference syntax`,
+	},
+	{
+		input: "[2001:db8::1]/repo",
+		wantRef: Reference{
+			Host:       "[2001:db8::1]",
+			Repository: "repo",
+		},
+	},
+	{
+		input: "[2001:db8:1:2:3:4:5:6]/repo:tag",
+		wantRef: Reference{
+			Host:       "[2001:db8:1:2:3:4:5:6]",
+			Repository: "repo",
+			Tag:        "tag",
+		},
+	},
+	{
+		input: "[2001:db8::1]:5000/repo",
+		wantRef: Reference{
+			Host:       "[2001:db8::1]:5000",
+			Repository: "repo",
+		},
+	},
+	{
+		input: "[2001:db8::1]:5000/repo:tag",
+		wantRef: Reference{
+			Host:       "[2001:db8::1]:5000",
+			Repository: "repo",
+			Tag:        "tag",
+		},
+	},
+	{
+		input: "[2001:db8::1]:5000/repo@sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		wantRef: Reference{
+			Host:       "[2001:db8::1]:5000",
+			Repository: "repo",
+			Digest:     "sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		},
+	},
+	{
+		input: "[2001:db8::1]:5000/repo:tag@sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		wantRef: Reference{
+			Host:       "[2001:db8::1]:5000",
+			Repository: "repo",
+			Tag:        "tag",
+			Digest:     "sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+		},
+	},
+	{
+		input: "[2001:db8::]:5000/repo",
+		wantRef: Reference{
+			Host:       "[2001:db8::]:5000",
+			Repository: "repo",
+		},
+	},
+	{
+		input: "[::1]:5000/repo",
+		wantRef: Reference{
+			Host:       "[::1]:5000",
+			Repository: "repo",
+		},
+	},
+	{
+		input:   "[fe80::1%eth0]:5000/repo",
+		wantErr: `invalid reference syntax`,
+	},
+	{
+		input:   "[fe80::1%@invalidzone]:5000/repo",
+		wantErr: `invalid reference syntax`,
+	},
+}
+
+func TestParseReference(t *testing.T) {
+	for _, test := range parseReferenceTests {
+		if test.testName == "" {
+			test.testName = test.input
+		}
+		t.Run(test.testName, func(t *testing.T) {
+			ref, err := ParseRelative(test.input)
+			t.Logf("ref: %#v", ref)
+			if test.wantErr != "" {
+				if test.wantErr == "invalid reference syntax" {
+					test.wantErr += regexp.QuoteMeta(fmt.Sprintf(" (%q)", test.input))
+				}
+				qt.Assert(t, qt.ErrorMatches(err, test.wantErr))
+				return
+			}
+			qt.Assert(t, qt.IsNil(err))
+			qt.Check(t, qt.Equals(ref, test.wantRef))
+			qt.Check(t, qt.Equals(ref.String(), test.input))
+			if test.wantRef.Host != "" {
+				ref1, err := Parse(test.input)
+				qt.Assert(t, qt.IsNil(err))
+				qt.Check(t, qt.Equals(ref1, test.wantRef))
+			} else {
+				_, err := Parse(test.input)
+				qt.Assert(t, qt.ErrorMatches(err, `reference does not contain host name`))
+			}
+		})
+	}
+}
