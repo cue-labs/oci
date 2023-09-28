c0d4887628f475e7511cbdea814d3e60e8bf4ef5
Paul Jolly <paul@myitcv.io>
2023-09-28 11:08:47 +0100

    licensing: fix up licensing and copyright
    Move ociregistry/LICENSE to the root of the repo. This Apache
    2.0 license (which we inherit from original code) applies to all Go
    modules in this repo. Per https://go.dev/ref/mod#vcs-license this then
    means the LICENSE applies to all modules in the repository:
    
        When the go command creates a .zip file for a module that is not in
        the repository root directory, if the module does not have a file
        named LICENSE in its root directory (alongside go.mod), the go
        command will copy the file named LICENSE from the repository root
        directory if it is present in the same revision.
    
    Add copyright headers to all .go and .cue files that do not have them.
    They are, by definition, files that have been added by work done by CUE
    Labs in the fork from original work.
    
    Leave original Google copyright headers in .go files that have them.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I8a4a9c0a9f7071033164d590e997b7b60d7d7ea8

--- ociregistry/ociref/reference.go
+++ ociregistry/ociref/reference.go
@@ -1,3 +1,17 @@
+// Copyright 2023 CUE Labs AG
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package ociref supports parsing cross-registry OCI registry references.
 package ociref
 
@@ -171,6 +185,7 @@ func isWord(c byte) bool {
 }
 
 // String returns the string form of a reference in the form
+//
 //	[HOST/]NAME[:TAG|@DIGEST]
 func (ref Reference) String() string {
 	var buf strings.Builder
