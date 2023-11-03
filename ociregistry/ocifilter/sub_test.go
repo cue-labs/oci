1b0e223dd293f24841f1499a1597b742cd75e3f2
Roger Peppe <rogpeppe@gmail.com>
2023-11-03 17:16:58 +0000

    ocifilter: make Sub pass auth scopes along correctly
    The scopes passed in by the caller will refer to repositories
    from the caller's perspective, but the underlying registry
    implementation expects them from its own perspective,
    so transform them as they're passed down.
    
    Also rename the source files, as "prefix.go" was a hangover
    from the previous API when the call was named `Prefix`.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I6d5f18faf7e18abb47dc2a0d4cda140375cda13e

--- ociregistry/ocifilter/sub_test.go
+++ ociregistry/ocifilter/sub_test.go
@@ -17,6 +17,7 @@ package ocifilter
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"sort"
 	"testing"
@@ -26,11 +27,12 @@ import (
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 
 	"cuelabs.dev/go/oci/ociregistry"
+	"cuelabs.dev/go/oci/ociregistry/ociauth"
 	"cuelabs.dev/go/oci/ociregistry/ocimem"
 	"cuelabs.dev/go/oci/ociregistry/ocitest"
 )
 
-func TestTrimPrefix(t *testing.T) {
+func TestSub(t *testing.T) {
 	ctx := context.Background()
 	r := ocitest.NewRegistry(t, ocimem.New())
 	r.MustPushContent(ocitest.RegistryContent{
@@ -100,6 +102,36 @@ func TestTrimPrefix(t *testing.T) {
 	qt.Assert(t, qt.IsNil(err))
 	sort.Strings(repos)
 	qt.Assert(t, qt.DeepEquals(repos, []string{"bar"}))
+}
+
+func TestSubMaintainsAuthScope(t *testing.T) {
+	var gotScope ociauth.Scope
+	r := Sub(contextChecker{
+		check: func(ctx context.Context) {
+			gotScope = ociauth.ScopeFromContext(ctx)
+		},
+	}, "foo/bar")
+	scope := ociauth.ParseScope("other registry:catalog:* repository:a/b:pull,push repository:foo:delete,push")
+	ctx := ociauth.ContextWithScope(context.Background(), scope)
+
+	// As the implementation is so uniform (and easily inspected in the source,
+	// we use the GetBlob entry point as a proxy for testing all the entry points.
+	// TODO it would be nice to have a reusable way (in ocitest, probably) of testing general properties
+	// across all ociregistry.Interface methods.
+	_, _ = r.GetBlob(ctx, "some/repo", "sha256:fffff")
+	qt.Assert(t, qt.DeepEquals(gotScope, ociauth.ParseScope(
+		"other registry:catalog:* repository:foo/bar/a/b:pull,push repository:foo/bar/foo:delete,push",
+	)))
+}
+
+type contextChecker struct {
+	ociregistry.Interface
+	check func(context.Context)
+}
+
+func (r contextChecker) GetBlob(ctx context.Context, repo string, digest ociregistry.Digest) (ociregistry.BlobReader, error) {
+	r.check(ctx)
+	return nil, fmt.Errorf("nope")
 }
 
 func getManifest(t *testing.T, r ociregistry.Interface, repo string, dg digest.Digest) ociregistry.Manifest {
