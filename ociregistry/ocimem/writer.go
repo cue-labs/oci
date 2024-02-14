f7f63c9e50d2d4b955e547ecf72d2e000e6f8924
Roger Peppe <rogpeppe@gmail.com>
2024-02-14 13:12:48 +0000

    ocimem: refactor content checks
    We refactor the content checking to use a more general
    form of iteration over references which we will also be
    able to use for checking that tag-referenced entities are
    not removed.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I4b922e7bcdce34b857870b18c4e8fd2355e4c507

--- ociregistry/ocimem/writer.go
+++ ociregistry/ocimem/writer.go
@@ -16,13 +16,11 @@ package ocimem
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 
 	"cuelabs.dev/go/oci/ociregistry"
 	"github.com/opencontainers/go-digest"
-	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 // This file implements the ociregistry.Writer methods.
@@ -119,7 +117,6 @@ func (r *Registry) PushManifest(ctx context.Context, repoName string, tag string
 		return ociregistry.Descriptor{}, fmt.Errorf("invalid manifest: %v", err)
 	}
 
-	// TODO check that all the layers in the manifest point to valid blobs.
 	repo.manifests[dig] = &blob{
 		mediaType: mediaType,
 		data:      data,
@@ -131,127 +128,38 @@ func (r *Registry) PushManifest(ctx context.Context, repoName string, tag string
 	return desc, nil
 }
 
-// TODO support other manifest types.
-var manifestCheckers = map[string]func(repo *repository, data []byte) (digest.Digest, error){
-	ocispec.MediaTypeImageManifest: manifestChecker(imageDescIter),
-	ocispec.MediaTypeImageIndex:    manifestChecker(indexDescIter),
-}
-
-func (r *Registry) checkManifest(repoName string, mediaType string, data []byte) (ociregistry.Digest, error) {
+func (r *Registry) checkManifest(repoName string, mediaType string, data []byte) (subject ociregistry.Digest, retErr error) {
 	repo, err := r.repo(repoName)
 	if err != nil {
 		return "", err
 	}
-	checker, ok := manifestCheckers[mediaType]
-	if !ok {
-		// TODO complain about non-manifest media types
-		return "", nil
+	iter, err := manifestReferences(mediaType, data)
+	if err != nil {
+		// TODO decide what to do about errUnknownManifestMediaTypeForIteration
+		return "", err
 	}
-	return checker(repo, data)
-}
-
-type digestCheck int
-
-const (
-	noCheck digestCheck = iota
-	blobMustExist
-	manifestMustExist
-)
-
-type descInfo struct {
-	name  string
-	desc  ocispec.Descriptor
-	check digestCheck
-}
-
-// manifestChecker returns a function that can be used to check manifests
-// that are JSON-unmarshaled into type T. The descIter function is
-// used to iterate over the descriptors inside T: it will be called with a yield
-// function that is called back to provide each descriptor, a name of the descriptor
-// and what check is appropriate for applying to that descriptor.
-func manifestChecker[T any](descIter func(T) func(yield func(descInfo) bool)) func(repo *repository, data []byte) (digest.Digest, error) {
-	return func(repo *repository, data []byte) (_ digest.Digest, retErr error) {
-		var x T
-		if err := json.Unmarshal(data, &x); err != nil {
-			return "", fmt.Errorf("cannot unmarshal into %T: %v", &x, err)
+	iter(func(info descInfo) bool {
+		if err := CheckDescriptor(info.desc, nil); err != nil {
+			retErr = fmt.Errorf("bad descriptor in %s: %v", info.name, err)
+			return false
 		}
-		iter := descIter(x)
-		var subject digest.Digest
-		iter(func(info descInfo) bool {
-			if info.name == "subject" {
-				subject = info.desc.Digest
-			}
-			if err := CheckDescriptor(info.desc, nil); err != nil {
-				retErr = fmt.Errorf("bad descriptor in %s: %v", info.name, err)
+		switch info.kind {
+		case kindBlob:
+			if repo.blobs[info.desc.Digest] == nil {
+				retErr = fmt.Errorf("blob for %s not found", info.name)
 				return false
 			}
-			switch info.check {
-			case blobMustExist:
-				if repo.blobs[info.desc.Digest] == nil {
-					retErr = fmt.Errorf("blob for %s not found", info.name)
-					return false
-				}
-			case manifestMustExist:
-				if repo.manifests[info.desc.Digest] == nil {
-					retErr = fmt.Errorf("manifest for %s not found", info.name)
-					return false
-				}
+		case kindManifest:
+			if repo.manifests[info.desc.Digest] == nil {
+				retErr = fmt.Errorf("manifest for %s not found", info.name)
+				return false
 			}
-			return true
-		})
-		return subject, retErr
-	}
-}
-
-func imageDescIter(m ociregistry.Manifest) func(yield func(descInfo) bool) {
-	return func(yield func(descInfo) bool) {
-		for i, layer := range m.Layers {
-			if !yield(descInfo{
-				name:  fmt.Sprintf("layers[%d]", i),
-				desc:  layer,
-				check: blobMustExist,
-			}) {
-				return
-			}
+		case kindSubjectManifest:
+			subject = info.desc.Digest
+			// The standard explicitly specifies that we can have
+			// a dangling subject so don't check that it exists.
 		}
-		if !yield(descInfo{
-			name:  "config",
-			desc:  m.Config,
-			check: blobMustExist,
-		}) {
-			return
-		}
-		if m.Subject != nil {
-			if !yield(descInfo{
-				name:  "subject",
-				desc:  *m.Subject,
-				check: noCheck,
-			}) {
-				return
-			}
-		}
-	}
-}
-
-func indexDescIter(m ocispec.Index) func(yield func(descInfo) bool) {
-	return func(yield func(descInfo) bool) {
-		for i, manifest := range m.Manifests {
-			if !yield(descInfo{
-				name:  fmt.Sprintf("manifests[%d]", i),
-				desc:  manifest,
-				check: manifestMustExist,
-			}) {
-				return
-			}
-		}
-		if m.Subject != nil {
-			if !yield(descInfo{
-				name:  "subject",
-				desc:  *m.Subject,
-				check: noCheck,
-			}) {
-				return
-			}
-		}
-	}
+		return true
+	})
+	return subject, retErr
 }
