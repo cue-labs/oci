cad7b0941809bb8a6e082e54b653e870a2f68d4f
Roger Peppe <rogpeppe@gmail.com>
2024-02-22 08:51:33 +0000

    ociclient: cope with omitted digest in tag GET response
    The AWS ECR registry does not return a Docker-Content-Digest
    header in its response when doing a GET of a tag.
    Technically this is conformant because the distribution
    spec says only that this "should" be returned in this case.
    
    However it is problematic because we need the digest to return
    in our API (because `BlobReader` exposes the digest).
    
    So we work around this by either reading the manifest if it's
    not too large, or by issuing a HEAD request if it is (technically
    the digest is also optional in the HEAD response, but in practice
    the ECR registry does return the digest in that response;
    other clients treat it as mandatory and fail if not present,
    and there's no other way to resolve a tag to a digest).
    
    Also add a way to get ociserver to emulate this quirk so we
    can test it.
    
    Updates #6
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I1694498a589fd029fc2a77e78ac864fd1ba9bba8

--- ociregistry/ociclient/reader.go
+++ ociregistry/ociclient/reader.go
@@ -15,12 +15,15 @@
 package ociclient
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 
 	"cuelabs.dev/go/oci/ociregistry"
 	"cuelabs.dev/go/oci/ociregistry/internal/ocirequest"
+	"github.com/opencontainers/go-digest"
 )
 
 func (c *client) GetBlob(ctx context.Context, repo string, digest ociregistry.Digest) (ociregistry.BlobReader, error) {
@@ -114,6 +117,18 @@ func (c *client) GetTag(ctx context.Context, repo string, tagName string) (ocire
 	})
 }
 
+// inMemThreshold holds the maximum number of bytes of manifest content
+// that we'll hold in memory to obtain a digest before falling back do
+// doing a HEAD request.
+//
+// This is hopefully large enough to be considerably larger than most
+// manifests but small enough to fit comfortably into RAM on most
+// platforms.
+//
+// Note: this is only used when talking to registries that fail to return
+// a digest when doing a GET on a tag.
+const inMemThreshold = 128 * 1024
+
 func (c *client) read(ctx context.Context, rreq *ocirequest.Request) (_ ociregistry.BlobReader, _err error) {
 	resp, err := c.doRequest(ctx, rreq)
 	if err != nil {
@@ -123,6 +138,49 @@ func (c *client) read(ctx context.Context, rreq *ocirequest.Request) (_ ociregis
 	desc, err := descriptorFromResponse(resp, ociregistry.Digest(rreq.Digest), true)
 	if err != nil {
 		return nil, fmt.Errorf("invalid descriptor in response: %v", err)
+	}
+	if desc.Digest == "" {
+		// Returning a digest isn't mandatory according to the spec, and
+		// at least one registry (AWS's ECR) fails to return a digest
+		// when doing a GET of a tag.
+		// We know the request must be a tag-getting
+		// request because all other requests take a digest not a tag
+		// but sanity check anyway.
+		if rreq.Kind != ocirequest.ReqManifestGet {
+			return nil, fmt.Errorf("internal error: no digest available for non-tag request")
+		}
+
+		// If the manifest is of a reasonable size, just read it into memory
+		// and calculate the digest that way, otherwise issue a HEAD
+		// request which should hopefully (and does in the ECR case)
+		// give us the digest we need.
+		if desc.Size <= inMemThreshold {
+			data, err := io.ReadAll(io.LimitReader(resp.Body, inMemThreshold+1))
+			if err != nil {
+				return nil, fmt.Errorf("failed to read body to determine digest: %v", err)
+			}
+			if int64(len(data)) != desc.Size {
+				return nil, fmt.Errorf("body size mismatch")
+			}
+			desc.Digest = digest.FromBytes(data)
+			resp.Body.Close()
+			resp.Body = io.NopCloser(bytes.NewReader(data))
+		} else {
+			rreq1 := rreq
+			rreq1.Kind = ocirequest.ReqManifestHead
+			resp1, err := c.doRequest(ctx, rreq1)
+			if err != nil {
+				return nil, fmt.Errorf(": %v", err)
+			}
+			resp1.Body.Close()
+			desc, err = descriptorFromResponse(resp1, "", true)
+			if err != nil {
+				return nil, err
+			}
+			if desc.Digest == "" {
+				return nil, fmt.Errorf("no digest header found in response")
+			}
+		}
 	}
 	return newBlobReader(resp.Body, desc), nil
 }
