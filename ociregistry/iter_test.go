3f134eda8caee3dff095ff4cb5c845fde9f3936c
Roger Peppe <rogpeppe@gmail.com>
2024-02-28 11:12:07 +0000

    ociregistry: add iterator test
    This is ensures that the new iterator type is in fact
    compatible with the range-over-func experiment.
    
    It's not tested in CI as yet, but I've made sure it
    works locally, and the likelihood of breakage seems
    very small given that the iterator type is encapsulated
    in `ociregistry.Seq`.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I17ea71d492cee91c5f5b6c617110104d711bec13

---
+++ ociregistry/iter_test.go
@@ -0,0 +1,32 @@
+//go:build go1.23 || goexperiment.rangefunc
+
+package ociregistry
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/go-quicktest/qt"
+)
+
+func TestSliceIter(t *testing.T) {
+	slice := []int{3, 1, 4}
+	var got []int
+	for x, err := range SliceIter(slice) {
+		qt.Assert(t, qt.IsNil(err))
+		got = append(got, x)
+	}
+	qt.Assert(t, qt.DeepEquals(got, slice))
+}
+
+func TestErrorIter(t *testing.T) {
+	err := errors.New("foo")
+	i := 0
+	for s, gotErr := range ErrorIter[string](err) {
+		qt.Assert(t, qt.Equals(i, 0))
+		qt.Assert(t, qt.Equals(s, ""))
+		qt.Assert(t, qt.Equals(err, gotErr))
+		i++
+	}
+	qt.Assert(t, qt.Equals(i, 1))
+}
