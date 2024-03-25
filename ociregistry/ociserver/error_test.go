ab83384cd1eb4528d9d43c06dcdfe3e8119bc694
Roger Peppe <rogpeppe@gmail.com>
2024-03-25 09:48:22 +0000

    ociregistry: improve errors with respect to HTTP status
    Currently the standard wire representation of an error is duplicated
    across both client and server, and there is no way for:
    - a client to access the actual HTTP status of a response
    - an `Interface` implementation to cause the `ociserver` to return
      a specific HTTP error code for a error code that it doesn't know
      about.
    
    This change addresses that by moving the wire representation into the
    top level `ociregistry` package, splitting `HTTPError` into its own
    type, and making both `ociclient` and `ociserver` aware of them.
    
    One deliberate decision made here guards against some scenarios when
    nesting an `ociclient` implementation inside `ociserver`. There is a
    risk that, due to the fact we're using the same `HTTPError` in
    `httpclient` and `httpserve, if `ociclient` talks to a misbehaving
    server that returns inappropriate HTTP response codes, those codes could
    propagate back through `ociserver`, causing that to return inappropriate
    codes too.
    
    So in `ociserver` we only use the `HTTPError` code if there is no known
    HTTP status for the error code. This seems like a reasonable
    half-way-house compromise.
    
    Fixes #26.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I5a79c1c6fec9f22c1f565830d73486b406dd181d

---
+++ ociregistry/ociserver/error_test.go
@@ -0,0 +1,69 @@
+// Copyright 2023 CUE Labs AG
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ociserver
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"cuelabs.dev/go/oci/ociregistry"
+
+	"github.com/go-quicktest/qt"
+)
+
+func TestHTTPStatusOverriddenByErrorCode(t *testing.T) {
+	r := New(&ociregistry.Funcs{
+		GetTag_: func(ctx context.Context, repo string, tagName string) (ociregistry.BlobReader, error) {
+			return nil, ociregistry.NewHTTPError(ociregistry.ErrNameUnknown, http.StatusUnauthorized, nil, nil)
+		},
+	}, nil)
+	s := httptest.NewServer(r)
+	defer s.Close()
+	resp, err := http.Get(s.URL + "/v2/foo/manifests/sometag")
+	qt.Assert(t, qt.IsNil(err))
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	qt.Assert(t, qt.Equals(resp.StatusCode, http.StatusNotFound))
+	qt.Assert(t, qt.JSONEquals(body, &ociregistry.WireErrors{
+		Errors: []ociregistry.WireError{{
+			Code_:   ociregistry.ErrNameUnknown.Code(),
+			Message: "401 Unauthorized: repository name not known to registry",
+		}},
+	}))
+}
+
+func TestHTTPStatusUsedForUnknownErrorCode(t *testing.T) {
+	r := New(&ociregistry.Funcs{
+		GetTag_: func(ctx context.Context, repo string, tagName string) (ociregistry.BlobReader, error) {
+			return nil, ociregistry.NewHTTPError(ociregistry.NewError("foo", "SOMECODE", nil), http.StatusTeapot, nil, nil)
+		},
+	}, nil)
+	s := httptest.NewServer(r)
+	defer s.Close()
+	resp, err := http.Get(s.URL + "/v2/foo/manifests/sometag")
+	qt.Assert(t, qt.IsNil(err))
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	qt.Assert(t, qt.Equals(resp.StatusCode, http.StatusTeapot))
+	qt.Assert(t, qt.JSONEquals(body, &ociregistry.WireErrors{
+		Errors: []ociregistry.WireError{{
+			Code_:   "SOMECODE",
+			Message: "418 I'm a teapot: foo",
+		}},
+	}))
+}
