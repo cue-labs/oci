2f19844df51e273a3079c512e88e8b4e03f5cbd8
Roger Peppe <rogpeppe@gmail.com>
2024-03-26 15:23:47 +0000

    ociregistry: improve errors with respect to HTTP status
    Currently the standard wire representation of an error is duplicated
    across both client and server, and there is no way for:
    - a client to access the actual HTTP status of a response
    - an `Interface` implementation to cause the `ociserver` to return
      a specific HTTP error status for an error code that it doesn't know
      about.
    
    This change addresses that by moving the wire representation into the
    top level `ociregistry` package, splitting `HTTPError` into its own
    type, and making both `ociclient` and `ociserver` aware of them.
    
    One deliberate decision made here guards against some scenarios when
    nesting an `ociclient` implementation inside `ociserver`. There is a
    risk that, due to the fact we're using the same `HTTPError` in
    `httpclient` and `httpserve, if `ociclient` talks to a misbehaving
    server that returns inappropriate HTTP response codes, those codes could
    propagate back through `ociserver`, causing that to return inappropriate
    codes too.
    
    So in `ociserver` we only use the `HTTPError` code if there is no known
    HTTP status for the error code. This seems like a reasonable
    half-way-house compromise.
    
    Fixes #26.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I5a79c1c6fec9f22c1f565830d73486b406dd181d

--- ociregistry/ociauth/auth.go
+++ ociregistry/ociauth/auth.go
@@ -12,6 +12,8 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"cuelabs.dev/go/oci/ociregistry"
 )
 
 // TODO decide on a good value for this.
@@ -298,8 +300,8 @@ func (r *registry) acquireAccessToken(ctx context.Context, requiredScope, wantSc
 	scope := requiredScope.Union(wantScope)
 	tok, err := r.acquireToken(ctx, scope)
 	if err != nil {
-		var rerr *responseError
-		if !errors.As(err, &rerr) || rerr.statusCode != http.StatusUnauthorized {
+		var herr ociregistry.HTTPError
+		if !errors.As(err, &herr) || herr.StatusCode() != http.StatusUnauthorized {
 			return "", err
 		}
 		// The documentation says this:
@@ -372,8 +374,8 @@ func (r *registry) acquireToken(ctx context.Context, scope Scope) (*wireToken, e
 		if err == nil {
 			return tok, nil
 		}
-		var rerr *responseError
-		if !errors.As(err, &rerr) || rerr.statusCode != http.StatusNotFound {
+		var herr ociregistry.HTTPError
+		if !errors.As(err, &herr) || herr.StatusCode() != http.StatusNotFound {
 			return tok, err
 		}
 		// The request to the endpoint returned 404 from the POST request,
@@ -448,34 +450,18 @@ func (r *registry) doTokenRequest(req *http.Request) (*wireToken, error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
+	data, bodyErr := io.ReadAll(resp.Body)
 	if resp.StatusCode != http.StatusOK {
-		return nil, errorFromResponse(resp)
+		return nil, ociregistry.NewHTTPError(nil, resp.StatusCode, resp, data)
 	}
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("cannot read response body: %v", err)
+	if bodyErr != nil {
+		return nil, fmt.Errorf("error reading response body: %v", err)
 	}
 	var tok wireToken
 	if err := json.Unmarshal(data, &tok); err != nil {
 		return nil, fmt.Errorf("malformed JSON token in response: %v", err)
 	}
 	return &tok, nil
-}
-
-type responseError struct {
-	statusCode int
-	msg        string
-}
-
-func errorFromResponse(resp *http.Response) error {
-	// TODO include body of response in error message.
-	return &responseError{
-		statusCode: resp.StatusCode,
-	}
-}
-
-func (e *responseError) Error() string {
-	return fmt.Sprintf("unexpected HTTP response %d", e.statusCode)
 }
 
 // deleteExpiredTokens removes all tokens from r that expire after the given
