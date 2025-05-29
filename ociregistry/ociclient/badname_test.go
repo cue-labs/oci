d0efc28a5723aa4fc6dbf7b4fa3e1d930aa6728e
Roger Peppe <rogpeppe@gmail.com>
2025-05-29 17:43:07 +0100

    internal/ocirequest: return ociregistry errors directly
    This means we don't need the shim layer in ociserver to decide
    what to do with the ocirequest errors, and ocirequest is itself
    already returning ociregistry errors, so there's no change
    in layering concerns here.
    
    This means that all errors returned from ocirequest.Parse
    have associated HTTP status codes, including the
    query parameter failure tested in the list_tags_bad_request test.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: Icc96d2fecee28852f5573e29935d2de7915d805f
    Reviewed-on: https://review.gerrithub.io/c/cue-labs/oci/+/1216085
    TryBot-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>

--- ociregistry/ociclient/badname_test.go
+++ ociregistry/ociclient/badname_test.go
@@ -19,9 +19,9 @@ func TestBadRepoName(t *testing.T) {
 	_, err = r.GetBlob(ctx, "Invalid--Repo", "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
 	qt.Check(t, qt.ErrorMatches(err, "invalid OCI request: name invalid: invalid repository name"))
 	_, err = r.GetBlob(ctx, "okrepo", "bad-digest")
-	qt.Check(t, qt.ErrorMatches(err, "invalid OCI request: badly formed digest"))
+	qt.Check(t, qt.ErrorMatches(err, "invalid OCI request: digest invalid: badly formed digest"))
 	_, err = r.ResolveTag(ctx, "okrepo", "bad-Tag!")
-	qt.Check(t, qt.ErrorMatches(err, "invalid OCI request: page not found"))
+	qt.Check(t, qt.ErrorMatches(err, "invalid OCI request: 404 Not Found: page not found"))
 }
 
 type noTransport struct{}
