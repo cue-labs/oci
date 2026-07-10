5eb5795f322ad5501a28ecedeb7e9956838fd8a6
Maximilian Gaß <m.gass@babiel.com>
2026-07-10 13:04:36 +0200

    ociregistry/ociserver: handle tags/list?n=0 correctly
    While here, remove unused variable and constant.
    
    Signed-off-by: Maximilian Gaß <m.gass@babiel.com>
    Change-Id: I9b95d2ce1d63e9d28dba880d9dffe2796a6a6964
    Reviewed-on: https://review.gerrithub.io/c/cue-labs/oci/+/1241558
    Reviewed-by: Roger Peppe <rogpeppe@gmail.com>
    TryBot-Result: CUE porcuepine <cue.porcuepine@gmail.com>

--- ociregistry/ociserver/lister.go
+++ ociregistry/ociserver/lister.go
@@ -31,8 +31,6 @@ import (
 	"cuelabs.dev/go/oci/ociregistry/internal/ocirequest"
 )
 
-const maxPageSize = 10000
-
 type catalog struct {
 	Repos []string `json:"repositories"`
 }
@@ -120,13 +118,16 @@ func (r *registry) handleReferrersList(ctx context.Context, resp http.ResponseWr
 }
 
 func (r *registry) nextListResults(req *http.Request, rreq *ocirequest.Request, itemsIter iter.Seq2[string, error]) (items []string, link string, _ error) {
+	// OCI Distribution spec for tags/list endpoint:
+	//     When n is zero, this endpoint MUST return an empty list, and MUST NOT include a Link header.
+	if rreq.ListN == 0 {
+		return []string{}, "", nil
+	}
+
 	if r.opts.MaxListPageSize > 0 && rreq.ListN > r.opts.MaxListPageSize {
 		return nil, "", ociregistry.NewError(fmt.Sprintf("query parameter n is too large (n=%d, max=%d)", rreq.ListN, r.opts.MaxListPageSize), ociregistry.ErrUnsupported.Code(), nil)
 	}
-	n := rreq.ListN
-	if n <= 0 {
-		n = maxPageSize
-	}
+
 	truncated := false
 	for item, err := range itemsIter {
 		if err != nil {
